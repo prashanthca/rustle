8bcd4bd0afe90be4d7444ac5cee420187f22fcc4
Prashanth C A <Prashanth@PRASHANTH-PC>
2015-08-02 20:51:28 +0530

    ASDA

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.$ = window.jQuery = require(__dirname+"/js/jquery.js");
+var remote = require("remote"),
+BrowserWindow = remote.require("browser-window");
+
+$(document).ready(function(){
+	$("#min-btn").click(function(){
+		var w = BrowserWindow.getFocusedWindow();
+		w.minimize();
+	});
+	$("#max-btn").click(function(){
+		var w = BrowserWindow.getFocusedWindow();
+		if(w.isMaximized())
+		{
+			w.unmaximize();
+		}
+		else
+		{
+			w.maximize();
+		}
+	});
+	$("#close-btn").click(function(){
+		var w = BrowserWindow.getFocusedWindow();
+		w.close();
+	});
+	$("#traffic-lights li").each(function(){
+		$(this).mouseover(function(){
+			$(this).addClass("icon-after");
+		}).mouseout(function(){
+			$(this).removeClass("icon-after");
+		});
+	});
+});
+
+$(document).keydown(function(e){
+	if(e.keyCode == 81 && e.ctrlKey == true) { 
+		BrowserWindow.getFocusedWindow().openDevTools();
+        return false;
+    }
+});
---
+++ main.js
@@ -0,0 +1,18 @@
+var app = require("app"),
+BrowserWindow = require("browser-window"),
+mainWindow = null;
+app.commandLine.appendSwitch('enable-transparent-visuals');
+app.on("ready", function () {
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 700,
+		frame: false,
+		show: false,
+		icon: __dirname+'/assets/icons/rustle2-256x256.png'
+	});
+	mainWindow.loadUrl("file://"+__dirname+"/index.html");
+	mainWindow.show();
+	mainWindow.on("close", function(){
+		mainWindow = null;
+	});
+});
