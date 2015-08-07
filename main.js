0cb19d2554c2fe82b3b71369bf39351a5e8dea3b
Prashanth C A <prashanth.c.a@hp.com>
2015-08-07 12:34:47 +0530

    07Aug

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 var app = require("app"),
 BrowserWindow = require("browser-window"),
 mainWindow = null;
-app.commandLine.appendSwitch('enable-transparent-visuals');
+//app.commandLine.appendSwitch('enable-transparent-visuals');
 app.on("ready", function () {
 	mainWindow = new BrowserWindow({
 		width: 1200,
 		height: 700,
-		frame: false,
 		show: false,
-		transparent: true,
-		icon: __dirname+'/assets/icons/rustle2-256x256.png'
+		frame: false,
+		icon: __dirname+'/assets/icons/rustleicon.png'
 	});
 	mainWindow.loadUrl("file://"+__dirname+"/index.html");
 	mainWindow.show();
