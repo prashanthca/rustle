e35f8fc75fa765da7ad7767444f17c75a83efa51
Prashanth <pc1@pca.vm>
2015-12-13 15:48:00 +0530

    New fonts

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ Tray = require('tray'),
 express = require('express')(),
 http = require('http'),
 server = http.Server(express),
-io = require("socket.io")(server);
+io = require("socket.io")(server),
+fs = require("fs"),
+file = __dirname+"/assets/rustle.db",
+exists = fs.existsSync(file),
+sqlite3 = require("sqlite3").verbose();
 
 express.get('/:str', function(req, res){
 	var rarr = {};
@@ -21,6 +25,11 @@ express.get('/', function(req, res){
 	res.send({"result":"error","message":"Invalid input"});
 });
 app.on("ready", function () {
+	if(!exists) {
+  		console.log("Creating DB file");
+		fs.openSync(file, "w");
+	}
+	var db = new sqlite3.Database(file);
 	mainWindow = new BrowserWindow({
 		width: 1200,
 		height: 700,
@@ -35,28 +44,35 @@ app.on("ready", function () {
 	mainWindow.on("close", function(){
 		mainWindow = null;
 	});
-  appIcon = new Tray(__dirname+'/assets/icons/rustle-tray2.png');
-  var contextMenu = Menu.buildFromTemplate([
-  { 
-  	label: 'Exit', 
-  	type: 'normal', 
-  	click: function(){
-  		mainWindow.close();
-  	} 
-  }
-  ]);
-  appIcon.on('double-clicked', function(){
-  	if(!mainWindow.isVisible())
-  		mainWindow.show();
-  });
-  appIcon.setToolTip('Rustle - Music Application');
-  appIcon.setContextMenu(contextMenu);
+	appIcon = new Tray(__dirname+'/assets/icons/rustle-tray2.png');
+	var contextMenu = Menu.buildFromTemplate([
+	{ 
+		label: 'Exit', 
+		type: 'normal', 
+		click: function(){
+			mainWindow.close();
+		} 
+	}
+	]);
+	appIcon.on('double-clicked', function(){
+		if(!mainWindow.isVisible())
+			mainWindow.show();
+	});
+	appIcon.setToolTip('Rustle - Music Application');
+	appIcon.setContextMenu(contextMenu);
+	db.serialize(function() {
+		if(!exists)
+		{
+			db.run("CREATE TABLE tracks (id INT(11) PRIMARY KEY, name VARCHAR(30) NOT NULL, artist VARCHAR(30) NOT NULL, length VARCHAR(50), added TIMESTAMP)");
+		}
+	});
+	db.close();
 });
 
 io.sockets.on('connection', function(socket){
-  console.log("Connection established");
+	console.log("Connection established");
 });
 
 server.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
